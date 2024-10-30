0dddf3a0bc6005ab6db226342ec44333f94d086c
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-10-30 18:41:32 -0400

    first try

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-from transformers import BertTokenizer, BertForSequenceClassification
-from transformers import pipeline
+import numpy as np
+import evaluate
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
 from datasets import load_dataset
 
-ds = load_dataset("zeroshot/twitter-financial-news-sentiment")
-print(ds)
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
 
-# model = BertForSequenceClassification.from_pretrained("ahmedrachid/FinancialBERT-Sentiment-Analysis",num_labels=3)
-# tokenizer = BertTokenizer.from_pretrained("ahmedrachid/FinancialBERT-Sentiment-Analysis")
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
 
-# nlp = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+dataset = load_dataset("zeroshot/twitter-financial-news-sentiment")
+tokenizer = AutoTokenizer.from_pretrained("ahmedrachid/FinancialBERT-Sentiment-Analysis")
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
-# sentences = ["Operating profit rose to EUR 13.1 mn from EUR 8.7 mn in the corresponding period in 2007 representing 7.7 % of net sales.",  
-#              "Bids or offers include at least 1,000 shares and the value of the shares must correspond to at least EUR 4,000.", 
-#              "Raute reported a loss per share of EUR 0.86 for the first half of 2009 , against EPS of EUR 0.74 in the corresponding period of 2008.",
-#              "the stock looks terrible",
-#              "the stock looks mid",
-#              ]
-# results = nlp(sentences)
-# print(results)
+small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(3000))
+small_eval_dataset = tokenized_datasets["validation"].shuffle(seed=42).select(range(300))
 
-# [{'label': 'positive', 'score': 0.9998133778572083},
-#  {'label': 'neutral', 'score': 0.9997822642326355},
-#  {'label': 'negative', 'score': 0.9877365231513977}]
+model = AutoModelForSequenceClassification.from_pretrained("ahmedrachid/FinancialBERT-Sentiment-Analysis", num_labels=3)
+
+metric = evaluate.load("accuracy")
+
+training_args = TrainingArguments(output_dir="test_trainer", eval_strategy="epoch", learning_rate=2e-5, per_device_train_batch_size=16, per_device_eval_batch_size=16, num_train_epochs=2, weight_decay=0.01)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=small_train_dataset,
+    eval_dataset=small_eval_dataset,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
