160e5fffc8a1cd37a52964f546e5058e089700c9
Raymond Chen <leaksrlifz@gmail.com>
2024-11-08 18:10:18 -0500

    twitkit works

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ EMAIL = 'devinlave02@gmail.com'
 PASSWORD = 'CMSC396HProject'
 
 # Initialize client
-client = Client('en-US')
+client = Client(user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36', language ='en-US')
 
 async def main():
     await client.login(
