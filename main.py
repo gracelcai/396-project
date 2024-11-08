4646af9bfcddcaf3c822b8ef990957dc8631db1f
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-11-08 18:11:32 -0500

    initial

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import evaluate
 
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
 from datasets import load_dataset
-
+ 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
@@ -23,7 +23,7 @@ model = AutoModelForSequenceClassification.from_pretrained("ahmedrachid/Financia
 
 metric = evaluate.load("accuracy")
 
-training_args = TrainingArguments(output_dir="test_trainer", eval_strategy="epoch", learning_rate=2e-5, per_device_train_batch_size=16, per_device_eval_batch_size=16, num_train_epochs=2, weight_decay=0.01)
+training_args = TrainingArguments(output_dir="test_trainer", eval_strategy="epoch", learning_rate=2e-5, per_device_train_batch_size=16, per_device_eval_batch_size=16, num_train_epochs=10, weight_decay=0.01)
 
 trainer = Trainer(
     model=model,
---
+++ scraper.py
@@ -0,0 +1,22 @@
+import asyncio
+from twikit import Client
+
+USERNAME = 'DevinLave43610'
+EMAIL = 'devinlave02@gmail.com'
+PASSWORD = 'CMSC396HProject'
+
+# Initialize client
+client = Client(
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
+)
+
+async def main():
+    await client.login(
+        auth_info_1=USERNAME ,
+        auth_info_2=EMAIL,
+        password=PASSWORD
+    )
+    
+    print(await client.get_trends('trending'))
+
+asyncio.run(main())
