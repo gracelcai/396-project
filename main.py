de6aae9346ae4fdc6bf2f0d38293ef0ebf8851ab
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-12-07 18:11:09 -0500

    add data to results.csv

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+import json
+import datetime
+from csv import writer
 from tweet_scraper import scrape
 from data_cleaning import clean_data
 from vader import vader_analyze
 from historical import get_stock_data
+
+f = open('scraper_config.json')
+config = json.load(f)
+symbol = config['symbols'][0][1:]
+
+tweet_csv = open('results.csv', 'a')
+writer_object = writer(tweet_csv)
 
 try:
     scrape()
 except:
     print("Rate limit exceeded. Please try again later.")
 clean_data()
-print("\nToday's score:", vader_analyze())
+
+vader_score = vader_analyze()
+print("\nToday's score:", vader_score)
 
 prices = get_stock_data()
-print("\nToday's opening price for NVDA:", prices['today_open'])
-# print("Yesterday's closing price:", prices['yesterday_close'])
+print(f"\nToday's opening price for {symbol}: {prices['today_open']}")
+
+writer_object.writerow([datetime.date.today(), symbol, vader_score, 'NA', prices['yesterday_close'], prices['today_open']])
+print("\nData saved to results.csv")
