0b6e27bd9520e1cf58ce847db1eccaf055949fd3
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-12-07 14:31:37 -0500

    scraper works now

--- scraper.py
+++ scraper.py
@@ -6,9 +6,7 @@ EMAIL = 'devinlave02@gmail.com'
 PASSWORD = 'CMSC396HProject'
 
 # Initialize client
-client = Client(
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
-)
+client = Client('en-US')
 
 async def main():
     await client.login(
