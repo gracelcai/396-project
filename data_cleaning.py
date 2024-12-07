ba065397d289d862597c4daf62e5f7c09bf5b229
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-12-07 16:25:11 -0500

    initial commit

---
+++ data_cleaning.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import json
+from datetime import datetime, timedelta
+
+def clean_data():
+    f = open('scraper_config.json')
+    config = json.load(f)
+
+    df = pd.read_csv('tweets.csv')
+    df.drop_duplicates(subset=["id"], inplace=True)
+
+    df = df[df['lang'] == 'en']
+
+    lookback = config['lookback_window']
+    cutoff_date = datetime.now() - timedelta(days=lookback)
+    df['time'] = df['time'].apply(lambda x: x[:-6])
+    df["time"] = pd.to_datetime(df["time"])
+    df = df[df['time'] >= cutoff_date]
+
+    df.to_csv('tweets.csv')
+
+    print("Data cleaning complete")
+    print("Number of usable tweets:", len(df))
