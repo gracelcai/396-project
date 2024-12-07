fc64ac50085a755abf48dfdf6567ab0a110baba1
Grace Cai <grace.cai7@gmail.com>
2024-12-07 18:56:11 -0500

    news scraping and finbert functions

---
+++ news_finbert.py
@@ -0,0 +1,42 @@
+import csv
+import argparse
+import pandas as pd
+from transformers import AutoModelForSequenceClassification
+
+from pytorch_pretrained_bert.modeling import BertForSequenceClassification
+import nltk
+import os
+
+nltk.download('punkt')
+
+# model = AutoModelForSequenceClassification.from_pretrained('./models/finbert', ignore_mismatched_sizes=True)
+
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+from news_scaper import *
+
+def news_results(symbol):
+    pipe = pipeline("text-classification", model="ProsusAI/finbert")
+    articles = get_news(symbol)
+    results = []
+    for article in articles['articles']:
+        text = article['title']
+        # text = article['description']
+        pred = pipe(text)[0]
+        article['label'] = pred['label']
+        article['score'] = pred['score']
+        results.append(article)
+    return results
+results = news_results('nvidia')
+print(results)
+# df = pd.read_csv('./news/nvidia.csv')
+
+# def predict(row):
+#     text = row['Title'].join(row['Description'])
+#     pred = pipe(text)
+#     return pred[0]['label'], pred[0]['score']
+
+# df[['label', 'score']] = df.apply(predict)
+
+# df.to_csv('./news/nvidia_results.csv')
+# return df.to_dict()
---
+++ news_scraper.py
@@ -0,0 +1,74 @@
+from newsapi import NewsApiClient
+import os
+import csv
+import argparse
+from dotenv import load_dotenv
+
+# load_dotenv()
+# parser=argparse.ArgumentParser()
+# parser.add_argument("symbol")
+# parser.add_argument("--filename")
+# args=parser.parse_args()
+
+# symbol = args.symbol
+# if args.filename:
+#    filename = args.filename
+# else:
+#     filename = symbol + ".csv"
+
+# # Create csv file
+# csvfile = open(os.path.join('news',filename), 'w', newline='')
+# writer = csv.writer(csvfile, delimiter=',')
+# writer.writerow(['Title', 'Description', 'Content', 'Source', 'URL', 'PublishedAt'])
+
+# # Get the value of an environment variable
+# api_key = os.environ.get("NEWS_API_KEY")
+# print(f"api key: {api_key}")
+# newsapi = NewsApiClient(api_key=api_key)  # Get an API key from https://newsapi.org/
+
+# # List of desired sources
+# sources = [
+#     # "the-motley-fool",
+#     # "the-wall-street-journal", 
+#     "bloomberg",
+#     "business-insider"
+# ]
+
+# # Get articles from these sources
+# articles = newsapi.get_everything(q=symbol,
+#                                 #   searchIn='title',
+#                                   sources=",".join(sources), 
+#                                   language="en", 
+#                                   from_param='2024-11-07',
+#                                   page_size=100)
+
+# # Print article details
+# for article in articles['articles']:
+#     writer.writerow([article['title'], article['description'], article.get('content', 'No content available'), article['source']['name'], article['url'], article['publishedAt']])
+#     print(f"Title: {article['title']}")
+#     print(f"Source: {article['source']['name']}")
+#     print(f"URL: {article['url']}\n")
+
+# print(newsapi.get_sources())
+
+def get_news(symbol):
+    load_dotenv()
+    api_key = os.environ.get("NEWS_API_KEY")
+    newsapi = NewsApiClient(api_key=api_key)  # Get an API key from https://newsapi.org/
+
+    # List of desired sources
+    sources = [
+        # "the-motley-fool",
+        # "the-wall-street-journal", 
+        "bloomberg",
+        "business-insider"
+    ]
+
+    # Get articles from these sources
+    articles = newsapi.get_everything(q=symbol,
+                                    #   searchIn='title',
+                                    sources=",".join(sources), 
+                                    language="en", 
+                                    from_param='2024-11-07',
+                                    page_size=100)
+    return articles
